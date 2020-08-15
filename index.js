c658c991addc4699e86a78c2c4a45eedead90868
Tiago Amaro <tiagopadrela@gmail.com>
2020-08-15 16:53:50 -0300

    Response for most common HTTP responses

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200)
-  res.end('Ola Toti!')
+  if (req.method === 'GET') {
+    res.writeHead(200)
+    res.end('<html><body><h1>Respondendo uma requisicao GET!</h1></body></html>')
+  } else if (req.method === 'POST') {
+    res.writeHead(200)
+    res.end('Respondendo requisicao POST!')
+  } else if (req.method === 'PUT') {
+    res.writeHead(200)
+    res.end('Respondendo requisicao PUT!')
+  } else if (req.method === 'DELETE') {
+    res.writeHead(200)
+    res.end('Respondendo requisicao DELETE!')
+  } else {
+    res.writeHead(400)
+    res.end(`Nao sei tratar esse metodo HTTP: ${req.method}`)
+  }
 })
 
 server.listen(8080)
